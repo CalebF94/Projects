5c574d1b024c86fc9d44104713174605e2a34897
CalebF94 <64437155+CalebF94@users.noreply.github.com>
2024-09-25 22:10:30 -0500

    Add files via upload

---
+++ Learning/Tree_Based_Methods/Tree_Based_Methods/config.py
@@ -0,0 +1,7 @@
+#from pathlib import Path
+import os
+
+## file variables
+plot_output = os.path.abspath("../Output/Plots")
+report_output = os.path.abspath("../Output/Reports")
+data_folder = os.path.abspath("../Data")
